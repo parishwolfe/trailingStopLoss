65e03b8a8702471387c901084e2de21872987596
Parish Wolfe <parishwolfe@gmail.com>
2021-08-04 12:00:40 -0400

    update

--- env_secret.py
+++ env_secret.py
@@ -7,6 +7,6 @@ alphavantage_key = os.environ.get("AV_KEY")
 
 twilio_sid = os.environ.get("TW_SID")
 twilio_token = os.environ.get("TW_TOKEN")
-twilio_number = "+1" + os.environ.get("TW_NUMBER")
+twilio_number = os.environ.get("TW_NUMBER")
 
-phone_number = "+1" + os.environ.get("PHONE_NUMBER")
+phone_number = os.environ.get("PHONE_NUMBER")
