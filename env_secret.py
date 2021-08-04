6acc364f4e38de47eb54c1256acc9c086f2637f1
Parish Wolfe <parishwolfe@gmail.com>
2021-08-04 11:56:56 -0400

    environment vars

--- env_secret.py
+++ env_secret.py
@@ -1,12 +1,12 @@
 import os
 
-username = os.environ.get('username')
-password = os.environ.get('password')
+username = os.environ.get('USERNAME')
+password = os.environ.get('PASSWORD')
 
-alphavantage_key = os.environ.get("AV_key")
+alphavantage_key = os.environ.get("AV_KEY")
 
-twilio_sid = os.environ.get("TW_sid")
-twilio_token = os.environ.get("TW_token")
-twilio_number = "+1" + os.environ.get("TW_number")
+twilio_sid = os.environ.get("TW_SID")
+twilio_token = os.environ.get("TW_TOKEN")
+twilio_number = "+1" + os.environ.get("TW_NUMBER")
 
-phone_number = "+1" + os.environ.get("phone_number")
+phone_number = "+1" + os.environ.get("PHONE_NUMBER")
