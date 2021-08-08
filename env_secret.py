d57b40c1b17d2f079fd7b90671c9749809251154
Parish Wolfe <parishwolfe@gmail.com>
2021-08-07 22:34:07 -0400

    Update env_secret.py

--- env_secret.py
+++ env_secret.py
@@ -12,5 +12,7 @@ twilio_number = os.getenv("TW_NUMBER")
 phone_number = os.getenv("PHONE_NUMBER")
 
 print("test", os.getenv("secrets.TEST"))
+for k, v in os.environ.items():
+    print(k)
 
 #cspell:ignore getenv twilio
