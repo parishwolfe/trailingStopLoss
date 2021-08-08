ec5806cfa9cae3ff25f35a9468026500af0bf7f7
Parish Wolfe <parishwolfe@gmail.com>
2021-08-07 22:03:22 -0400

    Update twilio.py

--- twilio.py
+++ twilio.py
@@ -27,9 +27,12 @@ def test():
     print("twilio status ", twilio_client.r.status_code)
     if twilio_client.r.status_code != 201:
         #raise Exception('Bad status code from twilio')
-        print(len(twilio_client.account_sid))
-        print(len(twilio_client.auth_token))
-        print(len(twilio_client.from_number))
+        if twilio_client.account_sid == None:
+            print("SID is None")
+        if twilio_client.auth_token == None:
+            print("Token is None")
+        if twilio_client.from_number == None:
+            print("From is None")
         pass
 
 if __name__ == '__main__':
